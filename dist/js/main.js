0eed5c4d0252e4f93d6fb888d37230692422dde0
iproject-leg <iproject@gmail.com>
2020-05-02 11:59:31 +0000

    Added testimonial, more app, and contact sections

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,3 +17,38 @@ var swiper = new Swiper("#home-testimonials .swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+//  Team members Swiper
+window.addEventListener("load", updateSwiperSize);
+document.addEventListener("DOMContentLoaded", updateSwiperSize);
+window.addEventListener("orientationchange", updateSwiperSize);
+window.addEventListener("resize", updateSwiperSize);
+
+function updateSwiperSize() {
+  let windowWidth = window.screen.width;
+  if (windowWidth <= 500) {
+    myTeamSwiper(1);
+  } else if (windowWidth > 500 && windowWidth <= 769) {
+    myTeamSwiper(2);
+  } else {
+    myTeamSwiper(3);
+  }
+}
+
+function myTeamSwiper(slidesPerView) {
+  var swiper = new Swiper("#home-how-it-works .tutorials .swiper-container", {
+    slidesPerView: slidesPerView,
+    spaceBetween: 30,
+    slidesPerGroup: slidesPerView,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
